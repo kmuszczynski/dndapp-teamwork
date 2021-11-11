7d61659947211a74c9aa0d971f3778237ddf746f
Lazaroks <cyprian.lazarowski@gmail.com>
2021-11-11 13:48:46 +0100

    + add reset password

--- dndapp_teamwork/settings.py
+++ dndapp_teamwork/settings.py
@@ -151,3 +151,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL='home'
 LOGIN_URL = 'login'
+
+#SMTP Configuration
+
+EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dnddjangoteamworkinf03@gmail.com'
+EMAIL_HOST_PASSWORD = 'Czat123.'
--- dndapp_teamwork/urls.py
+++ dndapp_teamwork/urls.py
@@ -13,6 +13,12 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
+    #reset password
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="users/resetpassword.html"), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="users/passwordsent.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="users/passwordresetconfirm.html"), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="users/passwordresetdone.html"), name='password_reset_complete'),
+
     # chat
     path('chat/', include('chat.urls')),
 
