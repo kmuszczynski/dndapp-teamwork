248f8061afaba6794b8198349f1c3970baef5f58
felufelu <=>
2022-07-02 15:07:51 +0200

    context tests done

--- test/test_AAContext.py
+++ test/test_AAContext.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.test import Client
 from charsheets.models import Character
 from request.models import UserToRoomRequest
+from grid.models import Grid, GridAreaWithCharacter
 from chat.models import ChatMessage, ChatRoom, UserBelongsToRoom
 from django.contrib.auth.models import User
 
@@ -9,40 +10,25 @@ class ContextTestCase(TestCase):
     def setUp(self):
         testuser = User.objects.create_user(username='testuser', password='12345')
         testuser2 = User.objects.create_user(username='testuser2', password='12345')
+        testuser3 = User.objects.create_user(username='testuser3', password='12345')
+        
         
         testroom = ChatRoom.objects.create(name="room1", gamemaster=testuser, status=1, description="desc 1")
         testchar1 = Character.objects.create(name="char1", level="0",race="race 1", combat_class="class 1", background="bg 1", alignment="align 1", room = testroom, user= testuser)
 
         req = UserToRoomRequest.objects.create(room=testroom,user=testuser2,message="msg")
-        
+
+        belong = UserBelongsToRoom.objects.create(room=testroom,user=testuser3)
+
         grid1 = Grid.objects.create(name="grid 1", columns=10, rows=10, status=1, room=testroom)
 
         panel1 = GridAreaWithCharacter.objects.create(column=1,row=1,grid=grid1, character="char1", user=testuser)
-
-
-# view room content
-
-    def test_Chat_Context_UserData(self):
-        user1 = User.objects.get(username="testuser")
-        client = Client()
-        login = client.login(username='testuser', password='12345')
-        response = client.get('/chat/room1/')
-        characters = Character.objects.get(user=user1)
-        self.assertEqual(response.context['playerCharacters'][0], characters)
-    
-    def test_Chat_Context_RoomData(self):
-        user1 = User.objects.get(username="testuser")
-        client = Client()
-        login = client.login(username='testuser', password='12345')
-        response = client.get('/chat/room1/')
-        messages = ChatMessage.objects.all()
-        self.assertEqual(len(response.context['chats']), len(messages))
 
 # view requests 
 
     def test_Requests_List(self):
         user1 = User.objects.get(username="testuser")
-        room1=ChatRoom.objects.get(name="room1")
+        room1 = ChatRoom.objects.get(name="room1")
         client = Client()
         login = client.login(username='testuser', password='12345')
         response = client.get('/request/view_all')
@@ -53,7 +39,14 @@ class ContextTestCase(TestCase):
 ## Write tests to check if request errors get through
     
 # list available/used rooms
-
+    def test_Rooms_List(self):
+        user1 = User.objects.get(username="testuser3")
+        room1= ChatRoom.objects.get(name="room1")
+        client = Client()
+        login = client.login(username='testuser3', password='12345')
+        response = client.get('/request/view_all_room')
+        rooms = UserBelongsToRoom.objects.filter(user=user1, room=room1)
+        self.assertEqual(response.context['player_rooms'][0], rooms[0])
     
 # list public rooms
 
@@ -65,11 +58,14 @@ class ContextTestCase(TestCase):
         rooms = ChatRoom.objects.all()
         self.assertEqual(response.context['rooms'][0], rooms[0])
 
-# chatroom entry
-
-
-
 # create character 
 
+    def test_Update_Character(self):
+        user1 = User.objects.get(username='testuser')
+        client = Client()
+        login = client.login(username='testuser', password='12345')
+        response = client.get('/character/update/1')
+        char = Character.objects.filter(user=user1)
+        self.assertEqual(response.context['character'], char[0])
 
 
